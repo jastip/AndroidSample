5d0c5a90c5032a3df13522f5b22c9eee1426a9b6
Yusak Setiawan <zivlab@Yusaks-MacBook-Pro.local>
2019-03-21 21:41:02 +0700

    Add Share to other app, and writing to clipboard

--- app/src/main/java/com/jastipapp/navigation/MainActivity.java
+++ app/src/main/java/com/jastipapp/navigation/MainActivity.java
@@ -2,21 +2,33 @@ package com.jastipapp.navigation;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -78,6 +90,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btnShare = (Button) findViewById(R.id.btnShare);
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSharing();
+            }
+        });
+
+        Button btnWriteClipboard = (Button) findViewById(R.id.btnWriteClipboard);
+
+        btnWriteClipboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeClipboard();
+            }
+        });
+
+    }
+
+    private void writeClipboard() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("UrlJastip", "http://www.google.com");
+        clipboard.setPrimaryClip(clip);
+    }
+
+    private void startSharing() {
+        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable.sampleimage);
+        File filesDir = getApplicationContext().getFilesDir();
+        File imageFile = new File(filesDir, "sampleimage.png");
+
+        OutputStream os;
+        try {
+            os = new FileOutputStream(imageFile);
+            bm.compress(Bitmap.CompressFormat.PNG, 100, os); // 100% quality
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+        }
+
+        Uri uri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID,imageFile);
+        Intent intent = new Intent();
+
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, "https://www.google.com");
+        intent.setType("image/png");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        writeClipboard();
+
+        startActivity(Intent.createChooser(intent, "Share this"));
     }
 
     private void startBluetoothIntent() {
